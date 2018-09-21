180a5df54d73b6440d9f40b46af130dcf031b423
joanne86 <LORETOladygaga1234>
2018-09-21 00:03:04 -0500

    Arreglando drawer

--- AdmConjutosResidenciales/src/main/java/co/edu/uan/controlador/CtrlPanelAdmin.java
+++ AdmConjutosResidenciales/src/main/java/co/edu/uan/controlador/CtrlPanelAdmin.java
@@ -13,6 +13,7 @@ import com.jfoenix.transitions.hamburger.HamburgerBackArrowBasicTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Accordion;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 
@@ -25,32 +26,40 @@ public class CtrlPanelAdmin implements Initializable{
     @FXML
     private JFXDrawer drawer;
 
+
+    @FXML
+    private Accordion acordeon;
+    
     @FXML
     private JFXHamburger hamburger;
     
+    static JFXDrawer drawer1;
+    static JFXHamburger hamburger1;
     public static AnchorPane rootP;
     
     public static AnchorPane panelMenu;
+    
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
+		hamburger1=hamburger;
 		rootP = root;
-        
+		drawer1=drawer;
         try {
         	panelMenu = FXMLLoader.load(getClass().getResource("/view/MenuAdmin.fxml"));
+        	panelMenu.setPrefHeight(java.awt.Toolkit.getDefaultToolkit().getScreenSize().height-45);
+        	//acordeon.setPrefHeight(java.awt.Toolkit.getDefaultToolkit().getScreenSize().height-288);
             drawer.setSidePane(panelMenu);
         } catch (IOException ex) {
             Logger.getLogger(CtrlPanelAdmin.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-        
-        
         HamburgerBackArrowBasicTransition transition = new HamburgerBackArrowBasicTransition(hamburger);
-        transition.setRate(-2);
+        transition.setRate(0);
         hamburger.addEventHandler(MouseEvent.MOUSE_PRESSED,(e)->{
+        	drawer.toFront();
             transition.setRate(transition.getRate()*-1);
             transition.play();
-            drawer.toFront();
+            
             if(drawer.isOpened())
             {
                 drawer.close();        
--- AdmConjutosResidenciales/src/main/java/co/edu/uan/controlador/CtrlPanelMenuAdmin.java
+++ AdmConjutosResidenciales/src/main/java/co/edu/uan/controlador/CtrlPanelMenuAdmin.java
@@ -11,27 +11,45 @@ import javafx.scene.layout.AnchorPane;
 
 public class CtrlPanelMenuAdmin {
 
-    @FXML
-    private JFXButton btnRegistroResidente;
-    
-    AnchorPane pane2;
-    
-    @FXML
-    void registrarPropietario(ActionEvent event) throws IOException {
-    	if (pane2 == null) {
-    		
+	@FXML
+	private JFXButton btnRegistroResidente;
+
+	static AnchorPane pane2;
+
+	static AnchorPane pane3;
+
+	@FXML
+	void registrarPropietario(ActionEvent event) throws IOException {
+		CtrlPanelAdmin.drawer1.close();
+		if (pane2 == null) {
+
 			pane2 = FXMLLoader.load(getClass().getResource("/view/Registro1.fxml"));
 			pane2.setLayoutX(0);
-			pane2.setLayoutY(0);
-			
+			pane2.setLayoutY(45);
+
 			CtrlPanelAdmin.rootP.getChildren().add(pane2);
-			pane2.toBack();
-			pane2.setPrefHeight(java.awt.Toolkit.getDefaultToolkit().getScreenSize().height);
+			pane2.setPrefHeight(java.awt.Toolkit.getDefaultToolkit().getScreenSize().height - 45);
 			pane2.setPrefWidth(java.awt.Toolkit.getDefaultToolkit().getScreenSize().width);
-			
 		} else {
-			pane2.toBack();
-			CtrlPanelAdmin.panelMenu.toFront();
+			pane2.toFront();
 		}
-    }
+	}
+
+	@FXML
+	void registrarVisitante(ActionEvent event) throws IOException {
+
+		CtrlPanelAdmin.drawer1.close();
+		if (pane3 == null) {
+
+			pane3 = FXMLLoader.load(getClass().getResource("/view/RegistroVisitantes.fxml"));
+			pane3.setLayoutX(0);
+			pane3.setLayoutY(45);
+			CtrlPanelAdmin.rootP.getChildren().add(pane3);
+			pane3.setPrefHeight(java.awt.Toolkit.getDefaultToolkit().getScreenSize().height - 45);
+			pane3.setPrefWidth(java.awt.Toolkit.getDefaultToolkit().getScreenSize().width);
+		} else {
+			pane3.toFront();
+		}
+	}
+
 }
